53c0b83d4292a0926483aa070a2c79c80ca5f616
allenk <ellenaponomarenko@gmail.com>
2023-09-26 19:20:01 +0300

    update bin

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+import greeting from '../src/cli.js';
+
+greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-function sayHello() {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
 }
 
-export default sayHello;
+export default greeting;
