6ca63f84fdb9005a7cce67e9eb0cb9d0b0f5790a
allenk <ellenaponomarenko@gmail.com>
2023-09-26 19:08:51 +0300

    added src and eslint

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+function sayHello() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+}
+
+export default sayHello;
