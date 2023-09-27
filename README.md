03d2c2308b784774ea278526032739a2dfef4241
allenk <ellenaponomarenko@gmail.com>
2023-09-27 16:57:26 +0300

    added brain-even game

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/alllenk1/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/alllenk1/frontend-project-44/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/c4b3ea3a3cbac318b560/maintainability)](https://codeclimate.com/github/alllenk1/frontend-project-44/maintainability)
---
+++ bin/brain-even.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const isEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+
+  const getRandom = () => Math.floor(Math.random() * 100);
+  let randomNum = getRandom();
+  let score = 0;
+  let wrongAnswer;
+  let correctAnswer;
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (randomNum % 2 === 0 && answer === 'yes') {
+      console.log('Correct!');
+      score += 1;
+    } else if (randomNum % 2 !== 0 && answer === 'no') {
+      console.log('Correct!');
+      score += 1;
+    } else {
+      wrongAnswer = answer;
+      if (wrongAnswer === 'no') {
+        correctAnswer = 'yes';
+      } else {
+        correctAnswer = 'no';
+      }
+      console.log(`'${wrongAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`);
+      break;
+    }
+    randomNum = getRandom();
+  }
+  if (score === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+isEvenGame();
