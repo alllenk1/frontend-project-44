b52bce19935676d041a4c2363c0d28243e278510
allenk <ellenaponomarenko@gmail.com>
2023-09-27 12:02:05 +0300

    added CodeClimate and updated Eslint

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
-}
+};
 
 export default greeting;
