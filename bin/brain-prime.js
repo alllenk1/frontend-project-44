444cadbcfdd24257e1d7339251318a8c294fc761
allenk <ellenaponomarenko@gmail.com>
2023-10-02 16:42:35 +0300

    added brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import start from '../src/games/prime.js';
+
+start();
---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import getRandomNum from '../random.js';
+import startGame from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (randomNum) => {
+  for (let i = 2; i < randomNum; i += 1) {
+    if (randomNum % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+isPrime();
+
+const isPrimeGame = () => {
+  const randomNum = getRandomNum(1, 10);
+  const question = `${randomNum}`;
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const start = () => startGame(isPrimeGame, description);
+
+export default start;
